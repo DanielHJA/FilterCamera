5626e9823901bf516f96b57ccf18fa859fe63a5d
Daniel <daniel.hjartstrom@gmail.com>
2020-03-20 08:56:56 +0100

    Initial Commit

---
+++ FilterCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilterCamera
+//
+//  Created by Daniel Hjärtström on 2020-03-20.
+//  Copyright © 2020 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
